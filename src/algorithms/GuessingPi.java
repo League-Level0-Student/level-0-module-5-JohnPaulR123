dae2e5436d39c8b336f258f7ac5bcd2c5791ab91
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-07-11 13:25:26 -0700

    For Loop Gauntlet,CircleRing, Guessing Pi

--- src/algorithms/GuessingPi.java
+++ src/algorithms/GuessingPi.java
@@ -14,11 +14,11 @@ public static void main(String[] args) {
 
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-String pi="3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862";
+String pi="15926535";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
-
+System.out.print("3.14");
 
 
 		// 9. If you want to give the user more than one chance to guess,
@@ -31,20 +31,22 @@ for(int i=0;i<pi.length();i++) {
 	
 
 				// 5. Ask the user for the NEXT digit of pi.
-String guess = JOptionPane.showInputDialog("Guess pi");
-				// 6. Compare the user's input to the next digit of your pi variable 
+String guess = JOptionPane.showInputDialog("Guess the Next Digit");
+		// 6. Compare the user's input to the next digit of your pi variable 
 				//    (look at step 3 for a clue).
 				//    HINT: use charAt(0) to get 1st char of user input String 
-int answer= Integer.parseInt(guess);
-if(answer==(pi.charAt(0))) {
+if(guess.equals(String.valueOf(pi.charAt(i)))) {
 	JOptionPane.showMessageDialog(null, "Correct");
-	
+	System.out.print(pi.charAt(i));
 }
 					// 7. If they are correct, print out "correct".
 
 					// 8. If they are not, print out "incorrect" and tell them
 					//    to start over. Use 'break;' to break out of the loop.
-
+else {
+	JOptionPane.showMessageDialog(null, "Incorrect");
+	break;
+}
 }
 }
 
