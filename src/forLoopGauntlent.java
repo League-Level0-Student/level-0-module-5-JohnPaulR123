515c67c3e927f547d2cbcf1f21d4c519e08cf8e4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-07-11 13:27:03 -0700

    for loop guantlet

--- src/forLoopGauntlent.java
+++ src/forLoopGauntlent.java
@@ -1,4 +1,29 @@
 
 public class forLoopGauntlent {
+	public static void main(String[] args) {
+		// 1. Display all numbers from 0 to 100
+		for(int a=0 ; a<101 ; a++) {
+			System.out.println(a);
+		}
+		// 2. Display all numbers from 100 to 0
+		for(int b=100 ; b>-1 ; b--) {
+			System.out.println(b);
+		}
+		// 3. Display all even numbers from 2 to 100
+		for(int c=2;c<101;c++) {
+			if(c)
+		}
+		// 4. Display all odd numbers from 1 to 99
+		
+		// 5. Display all numbers from 1 to 500. If the number is odd, print “odd” next
+		
+		// to the number. If the number is even, print “even” next to the number.
 
+		// 6. Display all multiples of 7 from 0 to 777.
+		
+		// 7. Print all the years you were alive and how old you were in each. e.g. “In
+		
+		// 1979, i was 2 years old.” (for a really old person)
+
+	}
 }
